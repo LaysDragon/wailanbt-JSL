f50327034475d81cd59ef1eacd9066d2e3c5b07a
LaysDragon <laysdra7265@gmail.com>
2014-12-16 15:56:57 +0800

    建立 用正規表達式把字串轉換成合法的json格式.js

---
+++ 單獨的function/將nbt物件toString解析成物件/用正規表達式把字串轉換成合法的json格式.js
@@ -0,0 +1,10 @@
+﻿function p(v){
+		v= v.replace(/^\\[/g, '{');
+		v= v.replace(/\\]$/g, '}');		
+		v= v.replace(/([0-9]+)(s|b|f|L|d)(,|})/g, '$1$3');
+		v= v.replace(/(,|{){1}([^{},:]+):{1}/g, '$1\"$2\":');
+		v= v.replace(/\\[(\\d+) bytes\\]/g, '[$1]');
+		v= v.replace(/,\\]/g, ']');
+		v = eval('('+v+')');
+			return v;
+		}
