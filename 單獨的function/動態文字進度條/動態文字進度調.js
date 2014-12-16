5e7df1347ac056cb6e183189b823f43e83d395e0
LaysDragon <laysdra7265@gmail.com>
2014-12-16 14:29:34 +0800

    上傳已完成的3個檔案 修正藥水效果為0的藥水沒加上顏色碼 藥水資料集補上顏色碼

---
+++ 單獨的function/動態文字進度條/動態文字進度調.js
@@ -0,0 +1,49 @@
+﻿//---------
+//物件animation
+//建構時傳入一個代表文字動畫的陣列
+//方法:
+//-getAnimationText
+//--參數:目前值,最大值
+//--返回:"目前值/最大值"百分比所代表的動畫文字
+//---------
+function animation(anT){
+		var animationArray = anT;
+		
+		this.getAnimationText = function (currectValue,maxValue){			
+			currectIndex = Math.floor((currectValue*(animationArray.length-1)/maxValue)>animationArray.length?animationArray.length-1:(currectValue*(animationArray.length-1)/maxValue));
+			//return currectIndex + '/'+ animationArray.length;
+			return  animationArray[currectIndex];
+		}
+	}
+
+//範例:
+function p(v){
+	function animation(anT){
+		var animationArray = anT;
+		
+		this.getAnimationText = function (currectValue,maxValue){			
+			currectIndex = Math.floor((currectValue*(animationArray.length-1)/maxValue)>animationArray.length?animationArray.length-1:(currectValue*(animationArray.length-1)/maxValue));
+			//return currectIndex + '/'+ animationArray.length;
+			return  animationArray[currectIndex];
+		}
+	}
+	//可自定義文字動畫陣列
+	var animationText = [	
+	'□□□□□□□□□□□□',
+	'■□□□□□□□□□□□',
+	'■■□□□□□□□□□□',
+	'■■■□□□□□□□□□',
+	'■■■■□□□□□□□□',
+	'■■■■■■□□□□□□',
+	'■■■■■■■□□□□□',
+	'■■■■■■■■□□□□',
+	'■■■■■■■■■□□□',
+	'■■■■■■■■■■□□',
+	'■■■■■■■■■■■□',
+	'■■■■■■■■■■■■',
+		];
+	//最大值
+	var maxTime = 200;
+	var animationTextObj = new animation(animationText);
+	return animationTextObj.getAnimationText(v,maxTime);
+}
---
+++ 單獨的function/動態文字進度條/釀造台專用進度條.js
@@ -0,0 +1,38 @@
+﻿//--------
+//參數:NBT BrewTime
+//返回:釀造台進度條
+//--------
+function p(v){
+	function animation(anT){
+		var animationArray = anT;
+		
+		this.getAnimationText = function (currectValue,maxValue){			
+			currectIndex = Math.floor((currectValue*(animationArray.length-1)/maxValue)>animationArray.length?animationArray.length-1:(currectValue*(animationArray.length-1)/maxValue));
+			//return currectIndex + '/'+ animationArray.length;
+			return  animationArray[currectIndex];
+		}
+	}
+	var animationText = [	
+	'□□□□□□□□□□□□',
+	'■□□□□□□□□□□□',
+	'■■□□□□□□□□□□',
+	'■■■□□□□□□□□□',
+	'■■■■□□□□□□□□',
+	'■■■■■■□□□□□□',
+	'■■■■■■■□□□□□',
+	'■■■■■■■■□□□□',
+	'■■■■■■■■■□□□',
+	'■■■■■■■■■■□□',
+	'■■■■■■■■■■■□',
+	'■■■■■■■■■■■■',
+		];
+	//建立全域變數CauldronTimeResetDelay來決定是否要重設進度調顯示以及重設延遲
+	if(typeof(CauldronTimeResetDelay)== 'undefined') CauldronTimeResetDelay =10 ;
+	var maxTime =400;
+	var animationTextObj = new animation(animationText);
+	var temp = CauldronTimeResetDelay;	
+	if((CauldronTimeResetDelay ==20) &&(v != 0))CauldronTimeResetDelay =0;
+	if((CauldronTimeResetDelay ==20) && (v == 0)) v = maxTime;
+	if(v == 0) CauldronTimeResetDelay++;
+	return '\u00A7f'+animationTextObj.getAnimationText(maxTime-v,maxTime)+'('+Math.floor(v/20)+' s)' ;
+}
